0cf8fd3735b67dfcbab2fe2e314aca2dec7d7c49
HoaNP <HoaNPSE03457@fpt.edu.vn>
2016-05-17 20:11:00 +0700

    Nghich ngu

--- Hello.cpp
+++ Hello.cpp
@@ -8,11 +8,26 @@
 
 #include <iostream>
 using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+ll add(ll a, ll b){
+	return(a+b);
+}
+
+ll multiply(ll a, ll b){
+	return(a*b);
+}
+
 /**
  * This is main function
  */
  //alo thinh da sua
 int main() {
-
+	ll a = 21;
+	ll b = 170;
+	cout << a << " + " << b << " = " << add(a,b) << endl;
+	cout << a << " * " << b << " = " << multiply(a,b) << endl;
 	return 0;
 }
