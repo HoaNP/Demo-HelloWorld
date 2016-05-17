1223884edf4a2345bd722835b1e9ff08c152bcaa
HoaNP <hoanpse03457@fpt.edu.vn>
2016-05-17 07:54:34 +0700

    First commit

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : HoaNP
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+/**
+ * This is main function
+ */
+int main() {
+
+	return 0;
+}
